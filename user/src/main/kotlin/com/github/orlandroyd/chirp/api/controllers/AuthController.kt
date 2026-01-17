921fe01bd6dd183b34997397ef9d0580f56681f7
OrlanDroyd <orlandroyd@gmail.com>
2026-01-17 12:57:42 +0000

    feat: add User Registration API Endpoint

---
+++ user/src/main/kotlin/com/github/orlandroyd/chirp/api/controllers/AuthController.kt
@@ -0,0 +1,27 @@
+package com.github.orlandroyd.chirp.api.controllers
+
+import com.github.orlandroyd.chirp.api.dto.RegisterRequest
+import com.github.orlandroyd.chirp.api.dto.UserDto
+import com.github.orlandroyd.chirp.api.mappers.toUserDto
+import com.github.orlandroyd.chirp.service.auth.AuthService
+import jakarta.validation.Valid
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/auth")
+class AuthController(private val authService: AuthService) {
+
+    @PostMapping("/register")
+    fun register(
+        @Valid @RequestBody body: RegisterRequest
+    ): UserDto {
+        return authService.register(
+            email = body.email,
+            username = body.username,
+            password = body.password
+        ).toUserDto()
+    }
+}
