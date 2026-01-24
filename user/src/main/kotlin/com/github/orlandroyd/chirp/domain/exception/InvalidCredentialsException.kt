a43490e6343f845cdbfd76286001a0dc48896805
OrlanDroyd <orlandroyd@gmail.com>
2026-01-24 23:27:24 +0000

    feat: add User Login Service Logic

---
+++ user/src/main/kotlin/com/github/orlandroyd/chirp/domain/exception/InvalidCredentialsException.kt
@@ -0,0 +1,5 @@
+package com.github.orlandroyd.chirp.domain.exception
+
+class InvalidCredentialsException: RuntimeException(
+    "The entered credentials aren't valid"
+)
---
+++ user/src/main/kotlin/com/github/orlandroyd/chirp/domain/exception/UserNotFoundException.kt
@@ -0,0 +1,3 @@
+package com.github.orlandroyd.chirp.domain.exception
+
+class UserNotFoundException: RuntimeException("User not found")
