f298fba0548ed7a983b7bc459d8dc7c6b85bbde0
OrlanDroyd <orlandroyd@gmail.com>
2026-02-22 11:45:03 +0000

    feat: Chat Participants Repository

--- chat/src/main/kotlin/com/github/orlandroyd/chirp/infra/database/entities/ChatMessageEntity.kt
+++ chat/src/main/kotlin/com/github/orlandroyd/chirp/infra/database/entities/ChatMessageEntity.kt
@@ -2,16 +2,7 @@ package com.github.orlandroyd.chirp.infra.database.entities
 
 import com.github.orlandroyd.chirp.domain.type.ChatId
 import com.github.orlandroyd.chirp.domain.type.ChatMessageId
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.FetchType
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
-import jakarta.persistence.Index
-import jakarta.persistence.JoinColumn
-import jakarta.persistence.ManyToOne
-import jakarta.persistence.Table
+import jakarta.persistence.*
 import org.hibernate.annotations.CreationTimestamp
 import java.time.Instant
 
@@ -32,7 +23,7 @@ class ChatMessageEntity(
     var id: ChatMessageId? = null,
     @Column(nullable = false)
     var content: String,
-    @JoinColumn(
+    @Column(
         name = "chat_id",
         nullable = false,
         updatable = false
@@ -46,7 +37,7 @@ class ChatMessageEntity(
         updatable = false
     )
     var chat: ChatEntity? = null,
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(
         name = "sender_id",
         nullable = false,
---
+++ chat/src/main/kotlin/com/github/orlandroyd/chirp/infra/database/repositories/ChatParticipantRepository.kt
@@ -0,0 +1,19 @@
+package com.github.orlandroyd.chirp.infra.database.repositories
+
+import com.github.orlandroyd.chirp.domain.type.UserId
+import com.github.orlandroyd.chirp.infra.database.entities.ChatParticipantEntity
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+
+interface ChatParticipantRepository : JpaRepository<ChatParticipantEntity, UserId> {
+    fun findByUserIdIn(userIds: List<UserId>): Set<ChatParticipantEntity>
+
+    @Query(
+        """
+        SELECT p
+        FROM ChatParticipantEntity p
+        WHERE LOWER(p.username) = :query OR LOWER(p.email) = :query
+    """
+    )
+    fun findByEmailOrUsername(query: String): ChatParticipantEntity?
+}
