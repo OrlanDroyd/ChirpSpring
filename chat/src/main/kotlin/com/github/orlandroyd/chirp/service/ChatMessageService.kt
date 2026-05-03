195b66e515a2bfdc99c27b13763e952c9dba4fb7
OrlanDroyd <orlandroyd@gmail.com>
2026-05-03 14:18:33 +0100

    feat: Fix Message Cache Eviction Bug

--- chat/src/main/kotlin/com/github/orlandroyd/chirp/service/ChatMessageService.kt
+++ chat/src/main/kotlin/com/github/orlandroyd/chirp/service/ChatMessageService.kt
@@ -29,7 +29,8 @@ class ChatMessageService(
     private val chatMessageRepository: ChatMessageRepository,
     private val chatParticipantRepository: ChatParticipantRepository,
     private val applicationEventPublisher: ApplicationEventPublisher,
-    private val eventPublisher: EventPublisher
+    private val eventPublisher: EventPublisher,
+    private val messageCacheEvictionHelper: MessageCacheEvictionHelper
 ) {
 
     @Transactional
@@ -92,14 +93,6 @@ class ChatMessageService(
             )
         )
 
-        evictMessagesCache(message.chatId)
-    }
-
-    @CacheEvict(
-        value = ["messages"],
-        key = "#chatId",
-    )
-    fun evictMessagesCache(chatId: ChatId) {
-        // NO-OP: Let Spring handle the cache evict
+        messageCacheEvictionHelper.evictMessagesCache(message.chatId)
     }
 }
---
+++ chat/src/main/kotlin/com/github/orlandroyd/chirp/service/MessageCacheEvictionHelper.kt
@@ -0,0 +1,16 @@
+package com.github.orlandroyd.chirp.service
+
+import com.github.orlandroyd.chirp.domain.type.ChatId
+import org.springframework.cache.annotation.CacheEvict
+import org.springframework.stereotype.Component
+
+@Component
+class MessageCacheEvictionHelper {
+    @CacheEvict(
+        value = ["messages"],
+        key = "#chatId",
+    )
+    fun evictMessagesCache(chatId: ChatId) {
+        // NO-OP: Let Spring handle the cache evict
+    }
+}
